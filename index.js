3273da25762f2693b30301f216d25f99e8032b72
Batagalage Dimuthu Lakshan <61883398+crazy-diya@users.noreply.github.com>
2022-10-11 01:24:19 +0530

    Update index.js

--- index.js
+++ index.js
@@ -98,39 +98,183 @@ const firestoreDB = admin.firestore();
 //     });
 
 
-exports.sendGoingEventNotification = functions.firestore
-    .document('/Users/{userId}/GoingList/{goingId}')
-    .onCreate(async (snapshot, context) => {
+// exports.sendGoingEventNotification = functions.firestore
+//     .document('/Users/{userId}/GoingList/{goingId}')
+//     .onCreate(async (snapshot, context) => {
 
 
-        let userRef = await firestoreDB.collection('Users').doc(context.params["userId"]).get();
-        const token = userRef.data().pushToken;
-        console.log(`token is : ${token}`);
+//         let userRef = await firestoreDB.collection('Users').doc(context.params["userId"]).get();
+//         const token = userRef.data().pushToken;
+//         console.log(`token is : ${token}`);
 
-        console.log(snapshot.data().description);
-        console.log(snapshot.data().time);
+//         console.log(snapshot.data().description);
+//         console.log(snapshot.data().time);
 
-        const dt = new Date(parseInt(snapshot.data().time));
-        const hr = dt.getHours();
-        const minute = dt.getMinutes();
-        const date = dt.getDate();
-        const month = dt.getMonth();
+//         const dt = new Date(parseInt(snapshot.data().time));
+//         const hr = dt.getHours();
+//         const minute = dt.getMinutes();
+//         const date = dt.getDate();
+//         const month = dt.getMonth();
 
-        console.log(`${hr} , ${minute} , ${date} , ${month}`);
+//         console.log(`${hr} , ${minute} , ${date} , ${month}`);
 
-        const payload = {
-            notification: {
-                title: snapshot.data().showName,
-                body: snapshot.data().description,
-                clickAction: 'FLUTTER_NOTIFICATION_CLICK',
-            }
-        };
+//         const payload = {
+//             notification: {
+//                 title: snapshot.data().showName,
+//                 body: snapshot.data().description,
+//                 clickAction: 'FLUTTER_NOTIFICATION_CLICK',
+//             }
+//         };
 
-        functions.pubsub.schedule('* * * * *').timeZone('IST').onRun(async context => {
-            console.log('I am running and executing every fucking 1:1 minute................');
-            await admin.messaging().sendToDevice(token, payload);
-            return Promise.resolve();
-        }); // THIS IS NOT WORKING
+//         functions.pubsub.schedule('* * * * *').timeZone('IST').onRun(async context => {
+//             console.log('I am running and executing every fucking 1:1 minute................');
+//             await admin.messaging().sendToDevice(token, payload);
+//             return Promise.resolve();
+//         }); // THIS IS NOT WORKING
 
-        return Promise.resolve();
+//         return Promise.resolve();
+//     });
+
+
+
+
+
+exports.scheduleFunction = functions.pubsub.schedule('0 8 * * *').timeZone('IST').onRun(async context => {
+    console.log('I am running and executing every fucking 1:1 minute................');
+
+    await firestoreDB.collection('Users').get().then(async (value) => {
+        for (let index = 0; index < value.docs.length; index++) {
+            const data = value.docs[index].id;
+
+            let userRef = await firestoreDB.collection('Users').doc(data).get();
+            const token = userRef.data().pushToken;
+            console.log(`token is : ${token}`);
+
+            await firestoreDB.collection('Users').doc(data).collection("GoingList").get().then(async (snapshot) => {
+                for (let index2 = 0; index2 < snapshot.docs.length; index2++) {
+                    const element = snapshot.docs[index2].id;
+
+                    let userRef2 = await firestoreDB.collection('Users').doc(data).collection("GoingList").doc(element).get();
+
+                    console.log(userRef2.data().description);
+
+                    console.log(userRef2.data().time);
+
+                    const currentYear = new Date().getFullYear();
+                    const currentMonth = new Date().getMonth();
+                    const currentDay = new Date().getDate();
+
+                    const dt = new Date(parseInt(userRef2.data().time));
+                    const hr = dt.getHours();
+                    const minute = dt.getMinutes();
+                    const date = dt.getDate();
+                    const month = dt.getMonth();
+
+                    console.log(`saved Date Details ${hr} , ${minute} , ${date} , ${month}`);
+                    console.log(`Current Date Details ${currentMonth} , ${currentDay} }`);
+
+
+                    if ((month === (currentMonth)) && (date === (currentDay + 1))) {
+                        console.log("Notification active!");
+                        const payload = {
+                            notification: {
+                                title: userRef2.data().showName,
+                                body: userRef2.data().description,
+                                clickAction: 'FLUTTER_NOTIFICATION_CLICK',
+                            }
+                        };
+                        await admin.messaging().sendToDevice(token, payload,);
+
+                    } else {
+                        console.log("Notification Not Activated!");
+                    }
+
+                }
+            });
+
+
+
+
+
+
+
+            // const payload = {
+            //     notification: {
+            //         title: snapshot.data().showName,
+            //         body: snapshot.data().description,
+            //         clickAction: 'FLUTTER_NOTIFICATION_CLICK',
+            //     }
+            // };
+            // await admin.messaging().sendToDevice(token, payload,);
+            // return Promise.resolve();
+
+
+        }
     });
+
+
+    return Promise.resolve();
+});
+
+
+
+
+
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:flutter/material.dart';
+
+import 'Home.dart';
+
+Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
+  // If you're going to use other Firebase services in the background, such as Firestore,
+  // make sure you call `initializeApp` before using other Firebase services.
+  await Firebase.initializeApp();
+
+  print("Handling a background message: ${message.messageId}");
+}
+
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHandler);
+
+  FirebaseMessaging messaging = FirebaseMessaging.instance;
+
+  NotificationSettings settings = await messaging.requestPermission(
+    alert: true,
+    announcement: false,
+    badge: true,
+    carPlay: false,
+    criticalAlert: false,
+    provisional: false,
+    sound: true,
+  );
+
+  print('User granted permission: ${settings.authorizationStatus}');
+  FirebaseMessaging.onMessage.listen((RemoteMessage message) {
+    print('Got a message whilst in the foreground!');
+    print('Message data: ${message.data}');
+
+    if (message.notification != null) {
+      print('Message also contained a notification: ${message.notification}');
+    }
+  });
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: "SYNCO",
+      home: Home(),
+      // home: NewEventView(),
+    );
+  }
+}
